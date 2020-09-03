375ced4514d377104ce9be1c879a283f0afe089e
李铭辉 <1018596741@qq.com>
2020-09-03 17:01:35 +0800

    feat(src/core/diretive.ts): 完成主体自定义指令逻辑编写

--- src/core/diretive.ts
+++ src/core/diretive.ts
@@ -1,5 +1,13 @@
 import {FunctionDirective} from "vue";
+import RoleCtr from "./instance";
 
 export const fnDirective:FunctionDirective<HTMLElement,string> = (el,binding,vnode) => {
-    
+    const targetByArg:string|undefined = binding.arg;
+    const targetByVal:string = binding.value;
+    let role:string = targetByArg?targetByArg:targetByVal;
+    if(RoleCtr.getRoles().length<=0) return
+    if(RoleCtr.match(role)===true){
+        el.parentNode?.removeChild(el)
+    }
+    return
 }
