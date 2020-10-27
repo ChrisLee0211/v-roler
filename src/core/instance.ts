ba172849c310bed4121011e902d4ca68a92f438f
李铭辉 <1018596741@qq.com>
2020-10-27 23:05:55 +0800

    feat: 权限控制器实例增加实例获取器逻辑

--- src/core/instance.ts
+++ src/core/instance.ts
@@ -1,38 +1,6 @@
+import { RoleInstanceClass } from "src/types/global";
 import {Ref, ref} from "vue";
 import {isArray,typeValidate} from "../utils";
-import {useRoler} from "./hook";
-
-/**
- * 构造一个控制角色列表的类，用于实现对权限的存放更新与判断操作
- * @author chrislee
- * @Time 2020/9/3
- */
-export interface RoleInstanceClass {
-    /**
-     * 获取当前已初始化的角色列表
-     */
-    getRoles():Ref<string[]>,
-    /**
-     * 初始化操作
-     * @param roles 角色数组
-     */
-    init(roles:string[]):void,
-    /**
-     * 动态添加角色权限
-     * @param role 角色数组或单一角色
-     */
-    addRole(role:string|string[]):Ref<string[]>
-    /**
-     * 更新角色列表
-     * @param roles 角色数组
-     */
-    update(roles:string[]):void,
-    /**
-     * 验证某一个角色是否存在角色列表中
-     * @param role 要验证的角色
-     */
-    match(role:string):boolean
-}
 
 class RoleInstance implements RoleInstanceClass {
     private roles:Ref<string[]> = [] as any;
@@ -91,32 +59,36 @@ class RoleInstance implements RoleInstanceClass {
  * @Time 2020/9/3
  */
 class RoleCtr implements RoleInstanceClass {
-    public ins:RoleInstanceClass | null = null;
- //
- //
- //
- //
- //这里还是需要改造成cl-flow那种先判断有没有生成实例的方式来代理
+    private ins:RoleInstanceClass | null = null;
+
     init(roles:string[]){
         if(this.ins===null){
-            this.ins = new RoleInstance(roles)
+            this.ins = new RoleInstance(roles);
         }
     }
 
     getRoles():Ref<string[]>{
-        return this.ins?.getRoles()??ref([])
+        return this.getInstance().getRoles()
     }
 
     update(roles:string[]){
-        this.ins?.update(roles)
+        this.getInstance().update(roles)
     }
 
     addRole(role:string|string[]):Ref<string[]>{
-       return this.ins?.addRole(role)??ref([])
+       return this.getInstance().addRole(role)
     }
 
     match(role:string):boolean{
-        return this.ins?.match(role)??false
+        return this.getInstance().match(role)
+    }
+
+    getInstance():RoleInstanceClass{
+        if(this.ins!==null){
+            return this.ins
+        }else{
+            throw new Error(`Fail to create the [v-roler] instance, please check the plugin if is installed `)
+        }
     }
 }
 
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,45 @@
-interface PluginOpt {
+import { Ref } from "_vue@3.0.2@vue";
+
+export interface PluginOpt {
     roles:string[],
     command?:string
 }
+
+/**
+ * 构造一个控制角色列表的类，用于实现对权限的存放更新与判断操作
+ * @author chrislee
+ * @Time 2020/9/3
+ */
+export interface RoleInstanceClass {
+    /**
+     * 获取当前已初始化的角色列表
+     */
+    getRoles():Ref<string[]>,
+    /**
+     * 初始化操作
+     * @param roles 角色数组
+     */
+    init(roles:string[]):void,
+    /**
+     * 动态添加角色权限
+     * @param role 角色数组或单一角色
+     */
+    addRole(role:string|string[]):Ref<string[]>
+    /**
+     * 更新角色列表
+     * @param roles 角色数组
+     */
+    update(roles:string[]):void,
+    /**
+     * 验证某一个角色是否存在角色列表中
+     * @param role 要验证的角色
+     */
+    match(role:string):boolean
+}
+
+declare module '@vue/runtime-core' {
+       interface ComponentCustomProperties {
+         $roles:string[];
+         $roler:Omit<RoleInstanceClass,"init">
+      }
+     }
