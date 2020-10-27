a99c8969cacf3fec82400f89ee7e71a816187277
李铭辉 <1018596741@qq.com>
2020-10-27 13:07:49 +0800

    feat: 新增mixin入口初始化，新增全局组件注册逻辑

--- src/core/init.ts
+++ src/core/init.ts
@@ -1,7 +1,29 @@
 import { App } from "vue";
 import RoleCtr from "./instance";
+import {initMixin} from "./mixin";
 
+import {rolerView} from "../components/rolerView";
+
+/**
+ * 初始化权限路由管理器及其工具
+ * @param {App} app 当前vue实例
+ * @param {PluginOpt} opts 插件配置项
+ * @author chrislee
+ * @Time 2020/10/27
+ */
 export default function initVRoler(app:App,opts:PluginOpt):App {
     RoleCtr.init(opts.roles);
+    initMixin(app);
+    registerComponent(app);
     return app
+}
+
+/**
+ * 注册全局组件
+ * @param app 
+ * @author chrislee
+ * @Time 2020/10/27
+ */
+function registerComponent(app:App){
+    app.component("roler-view",rolerView)
 }
