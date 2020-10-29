c2cf753918e56711cfbaf8f6aac33a044ad07bea
李铭辉 <1018596741@qq.com>
2020-10-29 22:22:04 +0800

    feat: 尝试配合forceUpdate实现动态插拔dom节点

--- src/core/directive.ts
+++ src/core/directive.ts
@@ -1,19 +1,39 @@
 import { PluginOpt } from "src/types/global";
-import {App, FunctionDirective, watch,} from "vue";
+import {App, FunctionDirective} from "vue";
 import RoleCtr from "./instance";
 
-export const fnDirective:FunctionDirective<HTMLElement,string> = (el,binding,vnode) => {
+const fnUpdate:FunctionDirective<HTMLElement,string> = (el,binding,vnode) => {
     const targetByArg:string|undefined = binding.arg;
     const targetByVal:string = binding.value;
     const role:string = targetByArg?targetByArg:targetByVal;
     if(RoleCtr.getRoles().length<=0) return
+    // if(binding.instance){
+    //     const dom = el;
+    //     RoleCtr.registerUpdateFn(dom,binding.instance.$forceUpdate);
+    // }
     if(RoleCtr.match(role)!==true){
-        el.parentNode?.removeChild(el);  
+        el.parentNode?.removeChild(el)
     }
+    // if(RoleCtr.match(role)!==true){
+    //     el.setAttribute("style","display:none")
+    //     el.style.display = "none"
+    // }else{
+    //     el.setAttribute("style","display:auto")
+    // }
+    return
+}
+
+const fnDestory:FunctionDirective<HTMLElement,string> = (el,binding) => {
+    RoleCtr.unregisterUpdateFn(el);
     return
 }
 
 export const initDirective = (app:App,opts:PluginOpt):App => {
     const command = opts.command??'can';
-    return app.directive(`${command}`,fnDirective)
+    return app.directive(`${command}`,{
+        mounted:fnUpdate,
+        updated:fnUpdate,
+        unmounted:fnDestory
+        
+    })
 }
